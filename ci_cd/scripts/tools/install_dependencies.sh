b55077df04c40cc7f718ddf9ef3bbccaa6da5be2
andMS <caro_ms00@hotmail.com>
2022-02-17 22:55:14 -0600

    [CI] testing pytest command

--- ci_cd/scripts/tools/install_dependencies.sh
+++ ci_cd/scripts/tools/install_dependencies.sh
@@ -4,5 +4,5 @@ if ! [ -x "$(command -v pytest)" ]; then
     echo "Pytest is not installed. Will do pytest installation"
     pip install pytest
     STATUS=$?
-    [$STATUS -eq 0 ] && echo "Pytest installation was successful." || echo "Could not install pytest."
+    [ $STATUS -eq 0 ] && echo "Pytest installation was successful." || echo "Could not install pytest."
 fi
